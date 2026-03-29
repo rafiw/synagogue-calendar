fab6ec80b75f5a80e27e752dbdc239d56a199b01
Rafi Wiener <rafiwiener@gmail.com>
2026-03-29 15:36:42 +0300

    fix: imporve Deceased responsivness

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity, useWindowDimensions } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { useSettings } from '../context/settingsContext';
 import { Ionicons } from '@expo/vector-icons';
 import { useRouter } from 'expo-router';

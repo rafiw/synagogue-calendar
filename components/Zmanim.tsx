c4d528363376423dbaf9938c67ed44f2cf26a8e3
Rafi Wiener <rafiwiener@gmail.com>
2026-04-13 23:49:07 +0300

    feat: usememo in zmanim

--- components/Zmanim.tsx
+++ components/Zmanim.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { ActivityIndicator, ScrollView, Text, View, useWindowDimensions } from 'react-native';
 import { FastDayType, ZmanimWrapper } from '../utils/zmanim_wrapper';
 import { useSettings } from '../context/settingsContext';
@@ -38,75 +38,168 @@ const Zmanim: React.FC = () => {
   const { width } = useWindowDimensions();
   const isSmallWidth = width < 500;
 
-  if (!i18n.isInitialized) {
-    return (
-      <View className="flex-1 justify-center items-center">
-        <ActivityIndicator size="large" color="#0000ff" />
-      </View>
-    );
-  }
-
   const [rtl, setRtl] = useState(false);
-  const zmanim = new ZmanimWrapper(
-    settings.synagogueSettings.nusach,
-    settings.zmanimSettings.latitude,
-    settings.zmanimSettings.longitude,
-    settings.zmanimSettings.olson,
-    settings.synagogueSettings.language,
-    settings.zmanimSettings.purimSettings,
-    settings.zmanimSettings.elevation,
+  const [timeTick, setTimeTick] = useState(() => Math.floor(Date.now() / 60000));
+
+  useEffect(() => {
+    const refreshTick = () => {
+      setTimeTick(Math.floor(Date.now() / 60000));
+    };
+
+    refreshTick();
+    const interval = setInterval(refreshTick, 30000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  const zmanim = useMemo(
+    () =>
+      new ZmanimWrapper(
+        settings.synagogueSettings.nusach,
+        settings.zmanimSettings.latitude,
+        settings.zmanimSettings.longitude,
+        settings.zmanimSettings.olson,
+        settings.synagogueSettings.language,
+        settings.zmanimSettings.purimSettings,
+        settings.zmanimSettings.elevation,
+      ),
+    [
+      settings.synagogueSettings.nusach,
+      settings.synagogueSettings.language,
+      settings.zmanimSettings.latitude,
+      settings.zmanimSettings.longitude,
+      settings.zmanimSettings.olson,
+      settings.zmanimSettings.purimSettings,
+      settings.zmanimSettings.elevation,
+      timeTick,
+    ],
   );
-  const hallelType = zmanim.getHallel();
-  const isShabbat = zmanim.getDOW() === 6;
-  const isFriday = zmanim.getDOW() === 5;
-  const tachanun = zmanim.getTachanun();
-  // if hallelType is not NO_HALLEL, don't show tachanun
-  const haveTachanun =
-    hallelType === HallelType.NO_HALLEL
-      ? getTachanunLabel(tachanun?.shacharit, tachanun?.mincha, tachanun?.allCongs, isShabbat)
-      : '';
+  const computedZmanim = useMemo(() => {
+    if (!i18n.isInitialized) {
+      return null;
+    }
+
+    const dow = zmanim.getDOW();
+    const isShabbat = dow === 6;
+    const isFriday = dow === 5;
+    const hallelType = zmanim.getHallel();
+    const tachanun = zmanim.getTachanun();
+    const haveTachanun =
+      hallelType === HallelType.NO_HALLEL
+        ? getTachanunLabel(tachanun?.shacharit, tachanun?.mincha, tachanun?.allCongs, isShabbat)
+        : '';
+    const fastDay = zmanim.isFastDay();
+
+    return {
+      dow,
+      hallelType,
+      haveTachanun,
+      fastDay,
+      haveCandleLighting: isFriday || isShabbat || zmanim.isHolidayCandleLighting(),
+      parsha: zmanim.getParsha(),
+      holidays: zmanim.getHoliday(),
+      hftara: zmanim.getHftara(),
+      megilla: zmanim.getMegilla(),
+      haveAlHanisim: zmanim.haveAlHanisim(),
+      haveYaaleVeyavo: zmanim.haveYaaleVeyavo(),
+      molad: zmanim.getMolad(),
+      omer: zmanim.getOmer(),
+      avotChapter: dow >= 5 ? zmanim.getAvotChapter() : '',
+      isMoridHatal: zmanim.isMoridHatal(),
+      isVetenBracha: zmanim.isVetenBracha(),
+      isSlichotTonight: zmanim.isSlichotTonight(),
+      times: {
+        alotHaShachar: zmanim.getAlotHaShachar(),
+        misheyakir: zmanim.getMisheyakir(),
+        netz: zmanim.getNetz(),
+        sofZmanShma: zmanim.getSofZmanShma(),
+        sofZmanShmaMGA: zmanim.getsofZmanShmaMGA(),
+        sofZmanTfilla: zmanim.getsofZmanTfilla(),
+        sofZmanTfillaMGA: zmanim.getsofZmanTfillaMGA(),
+        chatzot: zmanim.getChatzot(),
+        minchaGdola: zmanim.getMinchaGdola(),
+        minchaKtana: zmanim.getMinchaKtana(),
+        plag: zmanim.getPlag(),
+        sunset: zmanim.getSunset(),
+        tzeit: zmanim.gettzeit(),
+        candleLighting: zmanim.getCandleLighting(),
+        havdala: zmanim.getHavdala(),
+        havdalaRT: zmanim.getHavdalaRT(),
+        minorFastEnd: zmanim.getMinorFastEnd(),
+        majorFastEnd: zmanim.getMajorFastEnd(),
+      },
+    };
+  }, [i18n.isInitialized, zmanim]);
   const backgroundColor = 'bg-white/30';
   const headLineColor = 'text-gray-800';
   const textColor = 'text-gray-999';
-  const haveCandleLighting = isFriday || isShabbat || zmanim.isHolidayCandleLighting();
-  const fastDay = zmanim.isFastDay();
+  const dailyInfoItems = useMemo(() => {
+    if (!computedZmanim) {
+      return [];
+    }
 
-  // Build InfoGroup items array to check its size
-  const dailyInfoItems = [
-    ...(zmanim.getParsha() ? [{ text: t('parasha', { date: zmanim.getParsha() }) }] : []),
-    ...(zmanim.getHoliday() ? zmanim.getHoliday().map((holiday) => ({ text: t('holiday', { date: holiday }) })) : []),
-    ...(fastDay === FastDayType.MINOR_FAST
-      ? [
-          { text: t('fast_start', { date: zmanim.getAlotHaShachar() }) },
-          { text: t('fast_end', { date: zmanim.getMinorFastEnd() }) },
-        ]
-      : []),
-    ...(fastDay === FastDayType.MAJOR_FAST
-      ? [
-          { text: t('fast_start', { date: zmanim.getSunset() }) },
-          { text: t('fast_end', { date: zmanim.getMajorFastEnd() }) },
-        ]
-      : []),
-    ...(haveCandleLighting
-      ? [
-          { text: t('light_candle', { date: zmanim.getCandleLighting() }) },
-          { text: t('havdala', { date: zmanim.getHavdala(), date2: zmanim.getHavdalaRT() }) },
-        ]
-      : []),
-    ...(zmanim.getHftara() ? [{ text: t('hftara', { date: zmanim.getHftara() }) }] : []),
-    ...(zmanim.getMegilla() ? [{ text: t('megilla', { date: zmanim.getMegilla() }) }] : []),
-    ...(zmanim.haveAlHanisim() ? [{ text: t('al_hanisim') }] : []),
-    ...(zmanim.haveYaaleVeyavo() ? [{ text: t('yaale_veyavo') }] : []),
-    ...(zmanim.getMolad() ? [{ text: t('molad', { date: zmanim.getMolad() }) }] : []),
-    ...(zmanim.getOmer() ? [{ text: t('omer', { date: zmanim.getOmer() }) }] : []),
-    ...(zmanim.getDOW() >= 5 && zmanim.getAvotChapter() ? [{ text: zmanim.getAvotChapter() }] : []),
-    ...(zmanim.isMoridHatal() ? [{ text: t('morid_hatal') }] : [{ text: t('mashiv_haruach') }]),
-    ...(zmanim.isVetenBracha() ? [{ text: t('veten_bracha') }] : [{ text: t('tal_umatar') }]),
-    ...(hallelType === HallelType.WHOLE_HALLEL ? [{ text: t('whole_hallel') }] : []),
-    ...(hallelType === HallelType.HALF_HALLEL ? [{ text: t('half_hallel') }] : []),
-    ...(haveTachanun ? [{ text: t(haveTachanun) }] : []),
-    ...(zmanim.isSlichotTonight() ? [{ text: t('slichot') }] : []),
-  ];
+    return [
+      ...(computedZmanim.parsha ? [{ text: t('parasha', { date: computedZmanim.parsha }) }] : []),
+      ...(computedZmanim.holidays
+        ? computedZmanim.holidays.map((holiday) => ({ text: t('holiday', { date: holiday }) }))
+        : []),
+      ...(computedZmanim.fastDay === FastDayType.MINOR_FAST
+        ? [
+            { text: t('fast_start', { date: computedZmanim.times.alotHaShachar }) },
+            { text: t('fast_end', { date: computedZmanim.times.minorFastEnd }) },
+          ]
+        : []),
+      ...(computedZmanim.fastDay === FastDayType.MAJOR_FAST
+        ? [
+            { text: t('fast_start', { date: computedZmanim.times.sunset }) },
+            { text: t('fast_end', { date: computedZmanim.times.majorFastEnd }) },
+          ]
+        : []),
+      ...(computedZmanim.haveCandleLighting
+        ? [
+            { text: t('light_candle', { date: computedZmanim.times.candleLighting }) },
+            { text: t('havdala', { date: computedZmanim.times.havdala, date2: computedZmanim.times.havdalaRT }) },
+          ]
+        : []),
+      ...(computedZmanim.hftara ? [{ text: t('hftara', { date: computedZmanim.hftara }) }] : []),
+      ...(computedZmanim.megilla ? [{ text: t('megilla', { date: computedZmanim.megilla }) }] : []),
+      ...(computedZmanim.haveAlHanisim ? [{ text: t('al_hanisim') }] : []),
+      ...(computedZmanim.haveYaaleVeyavo ? [{ text: t('yaale_veyavo') }] : []),
+      ...(computedZmanim.molad ? [{ text: t('molad', { date: computedZmanim.molad }) }] : []),
+      ...(computedZmanim.omer ? [{ text: t('omer', { date: computedZmanim.omer }) }] : []),
+      ...(computedZmanim.avotChapter ? [{ text: computedZmanim.avotChapter }] : []),
+      ...(computedZmanim.isMoridHatal ? [{ text: t('morid_hatal') }] : [{ text: t('mashiv_haruach') }]),
+      ...(computedZmanim.isVetenBracha ? [{ text: t('veten_bracha') }] : [{ text: t('tal_umatar') }]),
+      ...(computedZmanim.hallelType === HallelType.WHOLE_HALLEL ? [{ text: t('whole_hallel') }] : []),
+      ...(computedZmanim.hallelType === HallelType.HALF_HALLEL ? [{ text: t('half_hallel') }] : []),
+      ...(computedZmanim.haveTachanun ? [{ text: t(computedZmanim.haveTachanun) }] : []),
+      ...(computedZmanim.isSlichotTonight ? [{ text: t('slichot') }] : []),
+    ];
+  }, [computedZmanim, t]);
+  const dayTimeItems = useMemo(() => {
+    if (!computedZmanim) {
+      return [];
+    }
+
+    return [
+      { text: t('shachar', { date: computedZmanim.times.alotHaShachar }) },
+      { text: t('mishayakir', { date: computedZmanim.times.misheyakir }) },
+      { text: t('netz', { date: computedZmanim.times.netz }) },
+      null,
+      { text: t('eo_shma', { date: computedZmanim.times.sofZmanShma }) },
+      { text: t('eo_shma_gra', { date: computedZmanim.times.sofZmanShmaMGA }) },
+      { text: t('eo_tfila', { date: computedZmanim.times.sofZmanTfilla }) },
+      { text: t('eo_tfila_gra', { date: computedZmanim.times.sofZmanTfillaMGA }) },
+      { text: t('chatzot', { date: computedZmanim.times.chatzot }) },
+      { text: t('mincha_gdola', { date: computedZmanim.times.minchaGdola }) },
+      { text: t('mincha_ktana', { date: computedZmanim.times.minchaKtana }) },
+      { text: t('plag_mincha', { date: computedZmanim.times.plag }) },
+      { text: t('sunset', { date: computedZmanim.times.sunset }) },
+      { text: t('stars', { date: computedZmanim.times.tzeit }) },
+    ];
+  }, [computedZmanim, t]);
 
   // Adjust heightScale based on InfoGroup items array size
   const heightScale = useHeightScale() * (dailyInfoItems.length > 10 ? 0.8 : dailyInfoItems.length > 8 ? 0.85 : 0.95);
@@ -172,31 +265,21 @@ const Zmanim: React.FC = () => {
     </View>
   );
 
+  if (!i18n.isInitialized) {
+    return (
+      <View className="flex-1 justify-center items-center">
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  }
+
   return (
     <ScrollView className="flex-1" contentContainerStyle={{ padding: padding / 2 }}>
       {/* Top Row */}
       <View className={isSmallWidth ? 'flex-col' : 'flex-row justify-between'} style={{ marginBottom: margin }}>
         <InfoGroup title={t('daily_info')} items={dailyInfoItems} />
 
-        <TimeGroup
-          title={t('day_times')}
-          items={[
-            { text: t('shachar', { date: zmanim.getAlotHaShachar() }) },
-            { text: t('mishayakir', { date: zmanim.getMisheyakir() }) },
-            { text: t('netz', { date: zmanim.getNetz() }) },
-            null,
-            { text: t('eo_shma', { date: zmanim.getSofZmanShma() }) },
-            { text: t('eo_shma_gra', { date: zmanim.getsofZmanShmaMGA() }) },
-            { text: t('eo_tfila', { date: zmanim.getsofZmanTfilla() }) },
-            { text: t('eo_tfila_gra', { date: zmanim.getsofZmanTfillaMGA() }) },
-            { text: t('chatzot', { date: zmanim.getChatzot() }) },
-            { text: t('mincha_gdola', { date: zmanim.getMinchaGdola() }) },
-            { text: t('mincha_ktana', { date: zmanim.getMinchaKtana() }) },
-            { text: t('plag_mincha', { date: zmanim.getPlag() }) },
-            { text: t('sunset', { date: zmanim.getSunset() }) },
-            { text: t('stars', { date: zmanim.gettzeit() }) },
-          ]}
-        />
+        <TimeGroup title={t('day_times')} items={dayTimeItems} />
       </View>
     </ScrollView>
   );
